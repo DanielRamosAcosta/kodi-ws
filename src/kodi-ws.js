6f6c2ace989401482d45cfe5a415a6d52b278b1b
Daniel Ramos <danielramosacosta@hotmail.com>
2016-08-09 01:46:46 +0100

    Hecha las pruebas de la funcion de conexión

--- src/kodi-ws.js
+++ src/kodi-ws.js
@@ -1,17 +1,15 @@
-var Connection = require('./Connection');
+import Connection from './Connection'
 
-function connect(host, port) {
-	return new Promise(function(resolve, reject) {
-		var connection = new Connection(host, port);
+export default function connect (host, port) {
+  return new Promise((resolve, reject) => {
+    const connection = new Connection(host, port)
 
-		connection.on('error', reject);
-		connection.on('connect', function() {
-			//Remove the handler so we dont try to reject on any later errors
-			connection.removeListener('error', reject);
+    connection.on('error', reject)
+    connection.on('connect', () => {
+      // Remove the handler so we dont try to reject on any later errors
+      connection.removeListener('error', reject)
 
-			resolve(connection);
-		});
-	});
+      resolve(connection)
+    })
+  })
 }
-
-module.exports = connect;
--- test/unit/kodi-ws.js
+++ test/unit/kodi-ws.js
@@ -1,10 +1,18 @@
 import KodiWS from '../../src/kodi-ws'
+import Connection from '../../src/Connection'
 
-describe('Connection', () => {
-  describe('#someMethod', () => {
-    it('does something useful', done => {
-      expect(false).to.equal(true)
-      done()
+describe('KodiWS', () => {
+  context('when the host and port are correct', () => {
+    it('resolves an instance of Connection', done => {
+      KodiWS('127.0.0.1', 9090).then(connection => {
+        expect(connection).to.be.an.instanceof(Connection)
+        done()
+      })
+    })
+  })
+  context('when an error occurs', () => {
+    it('rejects the promise', done => {
+      expect(KodiWS('192.168.100.100', 9090)).to.be.rejectedWith('Error: Timed out').and.notify(done)
     })
   })
 })
